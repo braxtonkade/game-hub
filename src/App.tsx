d88772151bf0b507bac0deca9c35b35c0339ebda
Braxton Dalton <braxtonkade1024@gmail.com>
2023-08-14 14:32:07 -0400

    Build Emoji; Update styling to GameCard & GameGrid

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string | null;
-  searchText: string;
+  searchText: string | null;
 }
 
 function App() {
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScoreBadge from "./CriticScoreBadge";
 import getCroppedImage from "../service/image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -13,13 +14,14 @@ const GameCard = ({ game }: Props) => {
     <Card key={game.id}>
       <Image src={getCroppedImage(game.background_image)} />
       <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
+        <HStack marginBottom={2} justifyContent={"space-between"}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScoreBadge score={game.metacritic} />
         </HStack>
+        <Heading fontSize={"2xl"}>{game.name}</Heading>
+        <Emoji rating={game.rating_top} />
       </CardBody>
     </Card>
   );
